d39041de73941817cf7ee1f0b91ab92a497428fe
Manos Kounelakis <pkounelios@gmail.com>
2019-03-10 13:57:12 +0200

    New tourist

--- traveller/tourist.py
+++ traveller/tourist.py
@@ -4,3 +4,6 @@ class Tourist():
 
     def say_hello(self):
         print("{} says hello.".format(self.name))
+
+    def say_magic_number(self, number):
+        print("{} says number {}".format(self.name, number * 2))
